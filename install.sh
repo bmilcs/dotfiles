5bca8d12e87cf566de4252d7921a2a29a07d6fe0
Bryan Miller <bmilcs@yahoo.com>
2022-10-19 15:48:52 -0400

    Add build i3-gaps to dev section

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# bmilcs dotfiles
+# ---------------------------
+# bmilcs dotfiles v3.0
+# - created in 10/2022
+# - utilizes stow for symlinking
+# ---------------------------
 
 #
 # configuration
@@ -10,28 +14,28 @@
 export DOT="$HOME"/repos/dotfiles
 backupPath="$HOME"/.backup
 
-# global dependencies
+# dependencies
 packages=(
-	curl
-	wget
-	git
-	stow
+  git
+  stow
 )
 
-# dev dependencies
 devPackages=(
+  i3-gaps
   alacritty
 )
 
-# configs to be installed (symlinked with stow utility)
+# dotfiles paths to install (via symlink with stow utility)
+
 currentUser=(
-	bash
-	nvim
   alacritty
+  bash 
+  i3
+  nvim
 )
 
 allUsers=(
-	git
+  git
 )
 
 
@@ -40,7 +44,7 @@ allUsers=(
 #
 
 # installs packages from an array of package names
-function installFromArray() {
+installFromArray() {
   array=($@)
   for i in "${array[@]}"; do
     # if command doesn't exist
@@ -56,31 +60,31 @@ done
 
 # installs dotfiles via symlink with stow utility, $1 destination, $2 source
 stowIt() {
-	destination=$1
-	sourcePath=$2
+  destination=$1
+  sourcePath=$2
 
-	# -v verbose
-	# -R restow
-	# -t target
+  # -v verbose
+  # -R restow
+  # -t target
   
   # executes stow & if error, catches package name into "conflict" variable
-	conflict=$(stow -v -R --adopt -t ${destination} ${sourcePath} 2>&1 |
-		awk '/\* existing target is/ {print $NF}')
+  conflict=$(stow -v -R --adopt -t ${destination} ${sourcePath} 2>&1 |
+    awk '/\* existing target is/ {print $NF}')
 
   # if error occured, attempt to back up the conflict
-	if [[ ! -z $conflict ]]; then
-		echo "conflicts found: ${conflict}"
-		echo "- moving conflicts to ${backupPath}"
-		mkdir -p "$backupPath"
-
-		for filename in ${CONFLICTS[@]}; do
-			if [[ -f $HOME/$filename || -L $HOME/$filename ]]; then
-				echo "BACKING UP: $filename"
-				mv "$HOME/$filename" $backupPath && \
-          echo "Backup successful. Re-run script to stow it again"
-			fi
-		done
-	fi
+  if [[ ! -z $conflict ]]; then
+    echo "conflicts found: ${conflict}"
+    echo "- moving conflicts to ${backupPath}"
+    mkdir -p "$backupPath"
+    
+    for filename in ${CONFLICTS[@]}; do
+      if [[ -f $HOME/$filename || -L $HOME/$filename ]]; then
+        echo "BACKING UP: $filename"
+        mv "$HOME/$filename" $backupPath && \
+         echo "Backup successful. Re-run script to stow it again"
+      fi
+    done
+  fi
 }
 
 #
@@ -96,14 +100,14 @@ installFromArray ${packages[@]}
 
 # install root & normal user stuff
 for app in ${allUsers[@]}; do
-	stowIt "${HOME}" "${app}"
+  stowIt "${HOME}" "${app}"
 done
 
 # install current user stuff only
 for app in ${currentUser[@]}; do
-	if [ "$EUID" -ne 0 ]; then
-		stowIt "${HOME}" "${app}"
-	fi
+  if [ "$EUID" -ne 0 ]; then
+    stowIt "${HOME}" "${app}"
+  fi
 done
 
 #
@@ -112,7 +116,7 @@ done
 
 # build neovim from source
 buildNeovim() {
-  read -p "Build Neovim? It takes a while!"
+  read -p "Build Neovim? (y/n) "
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     # dependencies
     sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
@@ -120,6 +124,21 @@ buildNeovim() {
     cd ~/repos/neovim &&
       make CMAKE_BUILD_TYPE=RelWithDebInfo &&
       sudo make install
+  fi
+}
+
+# build i3 gaps
+buildI3Gaps() {
+  read -p "Build i3 gaps? (y/n) "
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    destination=~/repos/i3-gaps
+    cd "$destination" || git clone https://github.com/Airblader/i3 "$destination"
+    sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+    cd "$destination"
+    mkdir -p build && cd build
+    meson --prefix /usr/local
+    ninja
+    sudo ninja install
   fi
 }
 
@@ -141,8 +160,9 @@ read -p "Setup DEV Environment?"
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "installing dev packages"
   # alacritty repo
-  sudo add-apt-repository ppa:aslatter/ppa
-  installFromArray ${devPackages[@]}
-	installFonts
-	buildNeovim
+  # sudo add-apt-repository ppa:aslatter/ppa
+  # installFromArray ${devPackages[@]}
+  buildI3Gaps
+  buildNeovim
+  installFonts
 fi
