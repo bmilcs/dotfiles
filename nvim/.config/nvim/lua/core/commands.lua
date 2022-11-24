de44246deac90f2c253f578a1ec0f231aaefe31c
Bryan Miller <bmilcs@yahoo.com>
2022-11-24 10:21:16 -0500

    Add nvim changes

--- nvim/.config/nvim/lua/core/commands.lua
+++
@@ -1,9 +0,0 @@
------------------------------------------------------------
--- Command functions
------------------------------------------------------------
-
-local command = function(command, target)
-	vim.api.nvim_create_user_command(command, target, {})
-end
-
-command("ToggleTerminal", [[:lua require("custom.toggleterm")()<cr>]]) -- custom toggleterm
--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++
@@ -1,6 +0,0 @@
------------------------------------------------------------
--- LSP Init configuration file
-----------------------------------------------------------
-
-require("plugins.lsp.lsp-installer")
-require("plugins.lsp.handlers").setup()
