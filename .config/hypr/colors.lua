eef29846b2b9a85e9216c614ab7c9b88d100a20e
bmilcs <bmilcsx@gmail.com>
2026-09-08 16:34:13 -0400

    Segment colors & appearance from hyprland conf

---
+++ .config/hypr/colors.lua
@@ -0,0 +1,30 @@
+local colors = {
+  backgroundRaw = "#282A36",
+  foregroundRaw = "#F8F8F2",
+  selectionRaw  = "#44475A",
+  commentRaw    = "#6272A4",
+  redRaw        = "#FF5555",
+  orangeRaw     = "#FFB86C",
+  yellowRaw     = "#F1FA8C",
+  greenRaw      = "#50FA7B",
+  purpleRaw     = "#BD93F9",
+  cyanRaw       = "#8BE9FD",
+  pinkRaw       = "#FF79C6",
+}
+
+colors.background = "rgb(40, 42, 54)"
+colors.foreground = "rgb(248, 248, 242)"
+colors.selection  = "rgb(68, 71, 90)"
+colors.comment    = "rgb(98, 114, 164)"
+colors.red        = "rgb(255, 85, 85)"
+colors.orange     = "rgb(255, 184, 108)"
+colors.yellow     = "rgb(241, 250, 140)"
+colors.green      = "rgb(80, 250, 123)"
+colors.purple     = "rgb(189, 147, 249)"
+colors.cyan       = "rgb(139, 233, 253)"
+colors.pink       = "rgb(255, 121, 198)"
+
+colors.accent     = colors.purple
+colors.accentRaw  = colors.purpleRaw
+
+return colors
