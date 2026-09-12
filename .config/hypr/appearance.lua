25ffc3193035642348cbb26a5ef5b7aa9c9d830b
bmilcs <bmilcsx@gmail.com>
2026-09-12 14:34:18 -0400

    Modify rounding

--- .config/hypr/appearance.lua
+++ .config/hypr/appearance.lua
@@ -19,7 +19,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
+        rounding       = 5,
         rounding_power = 2,
         active_opacity   = 1.0,
         inactive_opacity = 1.0,
