2d1aa68eaed06877e3823408572b28dba016d1e8
bmilcs <bmilcsx@gmail.com>
2026-09-12 17:22:41 -0400

    Add move window hotkeys

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -5,7 +5,6 @@ local colors = require("colors")
 ---- MONITORS ----
 ------------------
 
--- https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "",
     mode     = "preferred",
@@ -19,7 +18,7 @@ hl.monitor({
 
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local appMenu     = "hyprlauncher"
+local appMenu     = "rofi -show combi -combi-modi 'window,drun,ssh' -modi combi"
 local webBrowser  = "firefox"
 local youtubeMusic = webBrowser .. " --new-window https://music.youtube.com/playlist?list=LM"
 local classwork  = webBrowser .. " --new-window https://lms.protechskillsinstitute.org/"
@@ -31,7 +30,6 @@ local disc        = "discord"
 ---- AUTOSTART ----
 -------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function () 
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("systemctl --user start hyprland-session.target")
@@ -43,7 +41,6 @@ end)
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
 
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
@@ -54,8 +51,8 @@ hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
 
 hl.config({
     misc = {
-        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = -1,
+        disable_hyprland_logo   = false,
     },
 })
 
@@ -92,11 +89,11 @@ hl.gesture({
 
 local mainMod = "SUPER"
 local altMod = "CTRL"
+local triMod = "SHIFT"
 
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ mode = 0, action = "toggle" }))
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + W", hl.dsp.window.close())
--- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(webBrowser))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(youtubeMusic))
@@ -105,24 +102,29 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(battlenet))
 hl.bind(mainMod .. " + C", hl.dsp.exec_cmd(curseforge))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(appMenu))
+hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(appMenu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
-hl.bind(mainMod .. " + G", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + semicolon", hl.dsp.layout("togglesplit"))    -- dwindle only
 
--- Move focus with mainMod + arrow keys
+-- move focus with mainmod + arrow keys
 hl.bind(mainMod .. " + H",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + L",  hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + K",  hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
 
--- Resize focused window
+-- move focused window
+hl.bind(mainMod .. " + " .. triMod .. " + H", hl.dsp.window.move({direction = "left"}))
+hl.bind(mainMod .. " + " .. triMod .. " + L", hl.dsp.window.move({direction = "right"}))
+hl.bind(mainMod .. " + " .. triMod .. " + K", hl.dsp.window.move({direction = "right"}))
+hl.bind(mainMod .. " + " .. triMod .. " + J", hl.dsp.window.move({direction = "down"}))
+
+-- resize focused window
 hl.bind(mainMod .. " + " .. altMod .. " + L", hl.dsp.window.resize({ x = 10, y = 0, relative = true }), { repeating = true })
 hl.bind(mainMod .. " + " .. altMod .. " + H", hl.dsp.window.resize({ x = -10, y = 0, relative = true }), { repeating = true })
 hl.bind(mainMod .. " + " .. altMod .. " + K", hl.dsp.window.resize({ x = 0, y = -10, relative = true }), { repeating = true })
 hl.bind(mainMod .. " + " .. altMod .. " + J", hl.dsp.window.resize({ x = 0, y = 10, relative = true }), { repeating = true })
 
--- Switch workspaces with mainMod + [0-9]
--- Move active window to a workspace with mainMod + SHIFT + [0-9]
+-- switch workspaces with mainmod + [0-9]
 for i = 1, 9 do
     local key = i
     hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
@@ -133,19 +135,19 @@ end
 hl.bind(mainMod .. " + GRAVE", hl.dsp.focus({ workspace = 10}))
 hl.bind(mainMod .. " + SHIFT + GRAVE ", hl.dsp.window.move({ workspace = 10 }))
 
--- Example special workspace (scratchpad)
+-- example special workspace (scratchpad)
 hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
 
--- Scroll through existing workspaces with mainMod + scroll
+-- scroll through existing workspaces with mainmod + scroll
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
 
--- Move/resize windows with mainMod + LMB/RMB and dragging
+-- move/resize windows with mainmod + lmb/rmb and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
--- Audio control
+-- audio control
 -- volume up
 hl.bind(mainMod .. " + " .. altMod .. " + F", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
 -- volume down
@@ -203,15 +205,7 @@ hl.window_rule({
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
+-- hyprland-run windowrule
 hl.window_rule({
     name  = "move-hyprland-run",
     match = { class = "hyprland-run" },
@@ -219,7 +213,7 @@ hl.window_rule({
     float = true,
 })
 
--- WoW windowrule
+-- wow
 hl.window_rule({
     name = "world_of_warcraft",
     match = { title = "World of Warcraft" },
@@ -238,7 +232,7 @@ hl.window_rule({
     workspace = 1,
 })
 
--- Battle.net
+-- battle.net
 hl.window_rule({
     match = { title = "Battle.net" },
     immediate = false,
@@ -252,20 +246,18 @@ hl.window_rule({
     workspace = 3,
 })
 
---
--- HOST BASED CUSTOMIZATIONS
---
+-----------------------------------
+---- HOST BASED CUSTOMIZATIONS ----
+-----------------------------------
 
 -- acquire current hostname
 local handle = io.popen("cat /etc/hostname") 
 local hostname = handle:read("*a"):gsub("%s+", "") -- reads and removes whitespace
 handle:close()
 
--- host conditionals
-
 if hostname == "bmPC" then
 
-    -- DESKTOP WORKSPACES
+    -- desktop workspaces
     hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1" }) -- top monitor dedicated workspace
     hl.workspace_rule({ workspace = "1", gaps_out = 0, gaps_in = 0 }) -- gaming workspace
     for i = 1, 9 do
@@ -273,11 +265,11 @@ if hostname == "bmPC" then
     	hl.workspace_rule({ workspace = key, monitor = "DP-2" }) -- primary monitor
     end
 
-    -- DESKTOP MONITORS 
+    -- desktop monitors 
     hl.monitor({ output = "DP-2", mode = "3440x1440@59.97Hz", position = "0x0", scale = 1 })
     hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@59.95Hz", position = "515x-1440", scale = 1 })
 
-    -- DESKTOP LAUNCH APPS ON START
+    -- desktop launch apps on start
     hl.on("hyprland.start", function ()
       hl.exec_cmd(disc, { workspace = "10 silent" })
       hl.exec_cmd(youtubeMusic, { workspace = "10 silent" })
@@ -287,10 +279,10 @@ if hostname == "bmPC" then
 
 elseif hostname == "bmTP" then
 
-    -- LAPTOP MONITOR
+    -- laptop monitor
     hl.monitor({ output = "eDP-1", mode = "1920x1080@60.05Hz", position = "0x0", scale = 1 })
 
-    -- LAPTOP LAUNCH APPS ON START
+    -- laptop launch apps on start
     hl.on("hyprland.start", function ()
       hl.exec_cmd(disc, { workspace = "10 silent" })
       hl.exec_cmd(battlenet, { workspace = "3 silent" })
