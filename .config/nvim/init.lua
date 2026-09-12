71d9c0074efa853b3250a88ca910f7884a969b28
bmilcs <bmilcsx@gmail.com>
2026-09-12 16:42:07 -0400

    Change tab behavior in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,3 +11,9 @@ vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true, desc = 
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true, desc = "" })
 vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true, desc = "Yank to system clipboard" })
 vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true, desc = "" })
+
+-- tab behavior
+vim.opt.tabstop = 2      -- number of spaces that a <tab> in the file counts for
+vim.opt.softtabstop = 2  -- number of spaces that a <tab> counts for while performing editing operations
+vim.opt.shiftwidth = 2   -- number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- convert tabs to spaces
